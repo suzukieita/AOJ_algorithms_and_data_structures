3d9d6b2cb8d2359204652cbc8493acdd60e9e70e
suzuki eita <lingmuyingtai@gmail.com>
2019-06-21 15:55:19 +0900

    苦難の末の高速化に成功

--- chap14/DSL_2_C.rb
+++ chap14/DSL_2_C.rb
@@ -1,14 +1,12 @@
 class DTree
-  def initialize(p)
+  def initialize(p, n)
     # @p = p
-    n = p.size
     @t = Array.new(n)
     @np = 0
-    @sum = 0
-    makeDTree(0, n, 0, p)
+    makeDTree(0, n, p, 0)
   end
 
-  def makeDTree(l, r, depth, p)
+  def makeDTree(l, r, p, axis)
     unless l < r
       return nil
     end
@@ -17,81 +15,79 @@ class DTree
     t = @np
     @np += 1
 
-    
-    if depth % 2 == 0
-      new_p = p[l...r].sort_by{ |p| p.x }
-    else
-      new_p = p[l...r].sort_by{ |p| p.y }
-    end
+    new_p = p[l...r].sort_by{ |p| p[axis] }
     # puts "l: #{l}, r: #{r}, depth: #{depth} px: #{@p.map{|p| p.x}}, py: #{@p.map{|p| p.y}}"
     # puts "l: #{l}, r: #{r}, mid: #{mid}, new_p: #{new_p}"
     node = new_p[mid - l]
     @t[t] = node
-    @t[t].l = makeDTree(0, mid - l, depth + 1, new_p)
-    @t[t].r = makeDTree(mid+1 - l, r - l, depth + 1, new_p)
+    axis ^= 1
+    @t[t][3] = makeDTree(0, mid - l, new_p, axis)
+    @t[t][4] = makeDTree(mid+1 - l, r - l, new_p, axis)
 
     return t
   end
 
-  def find(v, sx, tx, sy, ty, depth, ans)
-    x = @t[v].x
-    y = @t[v].y
+  def find(v, sx, tx, sy, ty, ans, axis)
+    x = @t[v][0]
+    y = @t[v][1]
 
     if sx <= x && x <= tx && sy <= y && y <= ty
-      ans << @t[v].id
+      ans << @t[v][2]
     end
 
-    if depth % 2 == 0
-      if @t[v].l && sx <= x
-        find(@t[v].l, sx, tx, sy, ty, depth + 1, ans)
+    if axis.zero?
+      axis ^= 1
+      if @t[v][3] && sx <= x
+        find(@t[v][3], sx, tx, sy, ty, ans, axis)
       end
-      if @t[v].r && x <= tx
-        find(@t[v].r, sx, tx, sy, ty, depth + 1, ans)
+      if @t[v][4] && x <= tx
+        find(@t[v][4], sx, tx, sy, ty, ans, axis)
       end
     else
-      if @t[v].l && sy <= y
-        find(@t[v].l, sx, tx, sy, ty, depth + 1, ans)
+      axis ^= 1
+      if @t[v][3] && sy <= y
+        find(@t[v][3], sx, tx, sy, ty, ans, axis)
       end
-      if @t[v].r && y <= ty
-        find(@t[v].r, sx, tx, sy, ty, depth + 1, ans)
+      if @t[v][4] && y <= ty
+        find(@t[v][4], sx, tx, sy, ty, ans, axis)
       end
     end
   end
 end
 
-class MyNode
-  attr_accessor :x, :y, :id, :location, :l, :r
-  def initialize(id:, x:, y:)
-    @id = id
-    @x = x
-    @y = y
-    @location = nil
-    @l = nil
-    @r = nil
-  end
-end
+# MyNode is too late.
+# class MyNode
+#   attr_accessor :x, :y, :id, :l, :r
+#   def initialize(id:, x:, y:)
+#     @id = id
+#     @x = x
+#     @y = y
+#     @l = nil
+#     @r = nil
+#   end
+# end
 
-start_time = Time.now
+# start_time = Time.now
 n = gets.to_i
-p = Array.new(n)
+p = Array.new
 n.times do |i|
   x, y = gets.split.map(&:to_i)
-  p[i] = MyNode.new(id:i, x:x, y:y)
+  p << [x, y, i]
 end
 # p "Initialze #{Time.now - start_time}s"
 
-d_tree = DTree.new(p)
+d_tree = DTree.new(p, n)
 # p "Make tree #{Time.now - start_time}s"
 
 q = gets.to_i
 q.times do
   ans = []
   sx, tx, sy, ty = gets.split.map(&:to_i)
-  d_tree.find(0, sx, tx, sy, ty, 0, ans)
+  d_tree.find(0, sx, tx, sy, ty, ans, 0)
   ans.sort!
   ans.each do |id|
     puts id
   end
-  puts ''
+  puts
 end
 # p "End #{Time.now - start_time}s"
